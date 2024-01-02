b790e732b489ab08d31b5b0b52f4d5101a4ff26a
Tola-byte <adetolaisaiah17@gmail.com>
2024-01-02 13:40:58 +0100

    fix:flexbox fix

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -9,19 +9,19 @@ const Posts = () => {
     return (
         <main className="flex bg-black flex-col items-center justify-between p-24">
             <h3 className='text-2xl text-white font-extrabold items-start'>Blog Posts</h3>
-        <div className="flex flex-row flex-wrap w-[1200px] justify-center gap-5 p-4">
-        {
-                idAndTitleArray.map((item) => {
-                    return <Link href={`/posts/${item.id}`} key={item.id}>
+            <div className="flex flex-wrap max-w-[1200px] justify-center gap-5 p-5">
+                {idAndTitleArray.map((item) => (
+                    <Link href={`/posts/${item.id}`} key={item.id} >
                         <Cards
                             title={item.title}
                             author={item.author}
                             imgsrc={item.imgsrc}
-                            time={item.time} />
+                            time={item.time}
+                        />
                     </Link>
-            })
-                }
-                </div>
+                ))}
+            </div>
+
         </main>
     )
 }
